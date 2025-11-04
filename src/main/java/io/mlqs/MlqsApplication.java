3802a9acddde78d628ea4f7921d87cdb792030b8
YangPei <3455740700@qq.com>
2025-11-04 19:13:50 +0800

    初始化

---
+++ src/main/java/io/mlqs/MlqsApplication.java
@@ -0,0 +1,13 @@
+package io.mlqs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MlqsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MlqsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/mlqs/MlqsApplicationTests.java
@@ -0,0 +1,13 @@
+package io.mlqs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MlqsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
