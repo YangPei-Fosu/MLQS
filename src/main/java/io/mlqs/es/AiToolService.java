ecbae286b1a103487e7eb7540fd68a5b588b0198
YangPei <3455740700@qq.com>
2025-11-22 22:14:25 +0800

    Dev 0.0.13 Redis存在进程关闭再打开后记忆不完全的问题 FIXING

--- src/main/java/io/mlqs/es/AiToolService.java
+++ src/main/java/io/mlqs/es/AiToolService.java
@@ -1,5 +1,6 @@
 package io.mlqs.es;
 
+import dev.langchain4j.agent.tool.P;
 import dev.langchain4j.agent.tool.Tool;
 import dev.langchain4j.agent.tool.ToolMemoryId;
 import io.mlqs.es.entity.DocumentSearchResultEntity;
@@ -8,5 +9,5 @@ import java.util.Map;
 
 public interface AiToolService {
     @Tool("法律条文搜索工具，根据用户问题搜索相关的法律法规条文。参数context是查询的关键词或短句，参数legalName是一个字典，键是要搜索的法律文件名称，值是法条关联度阈值，只有高于阈值的搜索结果才会被返回")
-    DocumentSearchResultEntity search(@ToolMemoryId String sessionId, String context, Map<String, Double> legalName);
+    DocumentSearchResultEntity search(@ToolMemoryId String sessionId, @P("查询的关键词或短句") String context,@P("要搜索的法律文件名称与其关联度阈值") Map<String, Double> legalName);
 }
