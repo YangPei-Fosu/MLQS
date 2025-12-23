1346e71e86c4a6f6bf5d2c30328f491a8f35238a
刘晓虎 <1783226038@qq.com>
2025-12-23 11:34:28 +0800

    民法典和婚姻法

--- src/main/java/io/mlqs/es/db/repository/CivilCodeRecordRepository.java
+++
@@ -1,9 +0,0 @@
-package io.mlqs.es.db.repository;
-
-import io.mlqs.es.db.CivilCodeRecordEntity;
-import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CivilCodeRecordRepository extends ElasticsearchRepository<CivilCodeRecordEntity, String> {
-}
